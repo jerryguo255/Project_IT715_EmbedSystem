c0a79cd1b89d5f0798b0df795b354a91af740ee9
jerryguo255 <jerryguo255@outlook.com>
2019-10-25 18:14:53 +1300

    init project and test Serial Port check App

---
+++ TestSerial_ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace TestSerial_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get a list of serial port names.        
+            string[] ports = SerialPort.GetPortNames();
+            Console.WriteLine("The following serial ports were found:");
+            foreach (string port in ports) { Console.WriteLine(port); }
+            Console.ReadLine();
+        }
+    }
+}
